5e2091e461b782b33f93867e3a27164ec2a9f6e6
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-09 14:40:06 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,7 +8,7 @@ function addElementToBeginningOfArray(array,element){
 return([element,...array])
 }
 function destructivelyAddElementToBeginningOfArray(array,element){
-array.unshift(element);
+array.unshift(element)
 return(array)
 }
 function addElementToEndOfArray(array,element){
